a656b3781e2b271a107a583a21370dc81e6e750c
root <root@localhost.localdomain>
2018-06-27 07:37:05 +0100

    add file

---
+++ src/Ford/Fiesta/Fiesta2016.php
@@ -0,0 +1,8 @@
+<?php
+namespace Ford\Fiesta;
+
+class Fiesta2016{
+	public function info(){
+		echo 'this is fiesta2016';
+	}
+}
---
+++ src/Ford/Fusion/Fusion2016.php
@@ -0,0 +1,7 @@
+<?php
+namespace Ford\Fusion;
+class Fusion2016{
+	public function info(){
+		echo 'this is ford fusion2016';
+	}
+}
